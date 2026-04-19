9d54fe3885d17f162968db7233574bc0695253c1
Bora Demir <borad518@gmail.com>
2026-04-20 01:17:18 +0300

    istek log test

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("ISTEK GELDI:", req.method, req.url);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("ANA SAYFA CALISIYOR");
 });
 
 app.get("/test", (req, res) => {
   res.send("TEST TAMAM");
+});
+
+app.use((req, res) => {
+  res.status(404).send("EXPRESS 404 " + req.url);
 });
 
 const PORT = process.env.PORT || 3000;
