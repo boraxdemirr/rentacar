4d8c8384d2235ca94744ce3f6884f6810ef71996
Bora Demir <borad518@gmail.com>
2026-04-19 19:08:03 +0300

    test route

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.get("/test", (req, res) => {
+    res.send("test tamam");
+});
 
 // -------------------- VERİLER --------------------
 
